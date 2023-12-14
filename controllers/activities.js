843360b6cd38e7181ba9b8ca77b44fbd8da957c2
Pia <pia.koester@hey.com>
2023-12-14 15:48:54 +0100

    add get requests for all or single activity

--- controllers/activities.js
+++ controllers/activities.js
@@ -2,13 +2,14 @@ const Activity = require("../models/activities.js");
 
 const createActivity = async (req, res) => {
   try {
-    const { title, description, startTime, endTime, registeredUsers } = req.body;
+    const { title, description, startTime, endTime, registeredUsers } =
+      req.body;
     const activity = await Activity.create({
       title,
       description,
       startTime,
       endTime,
-      registeredUsers
+      registeredUsers,
     });
     res.status(201).json(activity);
   } catch (error) {
@@ -17,6 +18,29 @@ const createActivity = async (req, res) => {
   }
 };
 
+const getActivities = async (req, res) => {
+  try {
+    const activities = await Activity.find({});
+    res.json(activities);
+  } catch (error) {
+    console.log(error);
+    res.status(500).send("Error while getting activities");
+  }
+};
+
+const getActivity = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const activity = await Activity.findById(id);
+    res.json(activity);
+  } catch (error) {
+    console.log(error);
+    res.status(500).send("Error while getting activity");
+  }
+};
+
 module.exports = {
   createActivity,
+  getActivities,
+  getActivity,
 };
