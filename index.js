623f4612035ca838a648b81abbd42529401e1800
bishaldiganta <bishal.diganta@gmail.com>
2023-12-14 11:00:30 +0100

    add controller, models

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
-require("./db.js")
-const express = require('express')
-const app = express()
-const port = process.env.PORT
+require("./db.js");
+const express = require("express");
+const app = express();
+const port = process.env.PORT;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+//MiddleWare
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on http://localhost:${port}`)
-})
+  console.log(`Example app listening on http://localhost:${port}`);
+});
