51698bdf07e10bbce877e3fa4ab6f83b8469de54
Pia <pia.koester@hey.com>
2024-01-12 19:54:20 +0100

    change buttons to daisy ui buttons

--- client/src/components/UserProfile/UserUpdateInformation.jsx
+++ client/src/components/UserProfile/UserUpdateInformation.jsx
@@ -163,13 +163,13 @@ export default function UserUpdateInformation() {
 
                 <button
                   type="submit"
-                  className="w-full bg-primary text-white px-4 py-2 rounded-lg hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50 mb-2"
+                  className="w-full btn btn-primary text-white px-4 py-2  hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50 mb-2"
                 >
                   Update
                 </button>
                 <button
                   onClick={() => navigate(-1)}
-                  className="w-full bg-neutral text-white px-4 py-2 rounded-lg hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50"
+                  className="w-full btn btn-neutral text-white px-4 py-2  hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50"
                 >
                   Go back
                 </button>
