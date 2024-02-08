257c95673d1061c248e2adfc23a194661752ebe3
Pia <pia.koester@hey.com>
2024-02-08 22:38:08 +0100

    fix: remove error relating to old module

--- client/src/components/AdminProfile/Dashboard.jsx
+++ client/src/components/AdminProfile/Dashboard.jsx
@@ -64,24 +64,6 @@ export default function Dashboard() {
           >
             <nav>
               <Link
-                to={"/landingPage"}
-                class="block text-gray-500 py-2.5 px-4 my-4 rounded transition duration-200 hover:bg-gradient-to-r hover:from-cyan-500 hover:to-cyan-500 hover:text-white"
-              >
-                <span className="grid grid-cols-2 w-1/3">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 24 24"
-                    fill="currentColor"
-                    className="w-6 h-6 "
-                  >
-                    <path d="M11.47 3.841a.75.75 0 0 1 1.06 0l8.69 8.69a.75.75 0 1 0 1.06-1.061l-8.689-8.69a2.25 2.25 0 0 0-3.182 0l-8.69 8.69a.75.75 0 1 0 1.061 1.06l8.69-8.689Z" />
-                    <path d="m12 5.432 8.159 8.159c.03.03.06.058.091.086v6.198c0 1.035-.84 1.875-1.875 1.875H15a.75.75 0 0 1-.75-.75v-4.5a.75.75 0 0 0-.75-.75h-3a.75.75 0 0 0-.75.75V21a.75.75 0 0 1-.75.75H5.625a1.875 1.875 0 0 1-1.875-1.875v-6.198a2.29 2.29 0 0 0 .091-.086L12 5.432Z" />
-                  </svg>
-                  Home
-                </span>
-              </Link>
-
-              <Link
                 to={"/"}
                 class="block text-gray-500 py-2.5 px-4 my-4 rounded transition duration-200 hover:bg-gradient-to-r hover:from-cyan-500 hover:to-cyan-500 hover:text-white"
                 href="#"
@@ -355,7 +337,6 @@ export default function Dashboard() {
                         <th>Start Time</th>
                         <th>Edit</th>
                       </tr>
-
                     </thead>
                     {!activities ? (
                       <span className="loading loading-dots loading-lg"></span>
--- index.js
+++ index.js
@@ -27,7 +27,6 @@ app.use(
 );
 app.use(cookieParser());
 app.use(express.json({ limit: "200kb" }));
-//app.use(express.urlencoded({ extended: false }));
 app.use(bodyParser.urlencoded({ extended: false })); // this is an idea coming from stackoverflow
 app.use(express.static(path.resolve(__dirname, "client", "dist")));
 
